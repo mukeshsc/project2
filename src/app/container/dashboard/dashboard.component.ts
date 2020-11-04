0ffcc951f3f53d7ecf366f4290e1ec67a2687917
Kapil Karda <kapil.karda@gmail.com>
2020-11-04 19:01:59 +0530

    updates

--- src/app/container/dashboard/dashboard.component.ts
+++ src/app/container/dashboard/dashboard.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
 
+
+export class DashboardComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'designation', 'status'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+export interface PeriodicElement {
+  name: string;
+  position: any;
+  designation: string;
+  status: number;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: './assets/img/Ellipse_1.png', name: 'Romika gupta', designation: 'Sr. Associate', status: 0},
+  {position: './assets/img/Ellipse_1_g.png', name: 'Ryan Sandoval', designation: 'Manager Level 2', status: 1},
+  {position: './assets/img/Ellipse_1_gq.png', name: 'Fatima', designation: 'Director Manager', status: 2},
+  {position: './assets/img/Ellipse_1_gx.png', name: 'Romika gupta', designation: 'Sr. Associate', status: 0},
+  {position: './assets/img/Ellipse_1.png', name: 'Ryan Sandoval', designation: 'Manager Level 2', status: 1},
+  {position: './assets/img/Ellipse_1_g.png', name: 'Fatima', designation: 'Director Manager', status: 2},
+];
