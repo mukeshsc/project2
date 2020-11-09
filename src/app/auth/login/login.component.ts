1a3110c0ef7abd964972875aed31231bad305ce6
Kapil Karda <kapil.karda@gmail.com>
2020-11-09 19:17:10 +0530

    update code

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from 'src/app/service/auth-service.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import {
+  MatSnackBar
+} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  // from data for login api
+  formData = {
+    email: '',
+    password: ''
+  }
+  constructor(public _auth: AuthServiceService, private ngxService: NgxUiLoaderService, private _snackBar: MatSnackBar, public router:Router) { }
 
   ngOnInit(): void {
+
+    // Checking is login credential saved in remember me
+    if(localStorage.getItem('loginCred')){
+      this.formData = JSON.parse(localStorage.getItem('loginCred'));
+    }
+  }
+
+  // login api calling
+  async login(){
+    this.ngxService.start();
+    await(this._auth.login(this.formData).subscribe(res => {
+      this.ngxService.stop();
+      const response: any = res;
+      if (response.success == true){
+        localStorage.setItem('userData', JSON.stringify(response.data));
+        localStorage.setItem('token', response.data.tokens)
+        this.openSnackBar(response.message);
+        this.router.navigate(['/dashboard']);
+      }else{
+        this.openSnackBar(response.message);
+      }
+      console.log(res);
+    }));
+  }
+
+  // remember me to save login cred in cookies
+  rememberMe(e){
+    if (e.checked){
+      localStorage.setItem('loginCred', JSON.stringify(this.formData));
+    }else{
+      localStorage.setItem('loginCred', undefined);
+    }
+  }
+
+  // alert message after api response
+  openSnackBar(msg) {
+    this._snackBar.open(msg, 'Ok', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  // apiBaseUrl: 'http://a2b54c496d83.ngrok.io/',
+  apiBaseUrl: 'http://122.168.195.75:4000/',
+  apiPath: 'api/v1/superAdmin/',
 };
